25f2f74e03285d0ce5d5c001061a70c6787e3de7
Fernando Cejas <android10@fernandocejas.com>
2022-12-22 15:48:13 +0100

    feature: first compiling version of cli-quick-replace

--- 01-cli-quick-replace/src/main.rs
+++ 01-cli-quick-replace/src/main.rs
@@ -1,4 +1,38 @@
+use std::env;
+use text_colorizer::*;
+
 
 fn main() {
-    println!("Hello, world!");
+    let args = parse_arguments();
+    println!("{:?}", args);
+}
+
+fn print_cli_usage() {
+    eprintln!("{} - change ocurrences of one string into another.", "cli-quick-replace".green());
+    eprintln!("Usage: cli-quick-replace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_arguments() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_cli_usage();
+        eprintln!("{} wrong number of arguments: expected 4, got {}.", "Error: ".red().bold(), args.len());
+        std::process::exit(1);
+    }
+
+    Arguments { 
+        target: args[0].clone(), 
+        replacement: args[1].clone(), 
+        filename: args[2].clone(), 
+        output: args[3].clone()
+    }
+}
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
 }
